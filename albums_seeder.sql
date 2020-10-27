1067e86bdb1f97ee2d6d64143f32efcf881114a3
RMKendall <ryan.morris.kendall@gmail.com>
2020-10-27 11:50:42 -0500

    Complete the mysql Update exercises

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,5 +1,7 @@
 USE codeup_test_db;
 
+Truncate albums;
+
 insert into albums(artist, name, release_date, genre, sales)
 values('Michael Jackson', 'Thriller', 1982, 'Pop, Rock, R&B', 47.3),
        ('AC/DC', 'Back in Black', 1980, 'Hard rock', 26.1),
